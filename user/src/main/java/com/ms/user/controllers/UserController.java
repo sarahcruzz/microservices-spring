dfe81f9b22bde5dbdb12e39eb41da4531102bb9d
sarahcruzz <sarah.cruz0492@gmail.com>
2024-07-16 14:16:19 -0300

    andamento

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.ms.user.controllers;
 
+import com.ms.user.dtos.UserRecordDTO;
 import com.ms.user.models.UserModel;
+import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO){
-
-
-        return ResponseEntity.status(HttpStatus.CREATED).body();
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDTO, userModel); // faz a conversão de dto para model ser salvo na base de dados
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
     }
 
 
---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,18 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
+    public UserModel save(UserModel userModel){
+        return  userRepository.save(userModel);
+    }
+}
